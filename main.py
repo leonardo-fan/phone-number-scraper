5a85a6f3ae77a904cbb9e373d9c0923f6d5bca1c
Leonardo Fan <leonardofan2000@gmail.com>
2023-10-28 01:26:09 +1100

    implement business logic for basic search

--- main.py
+++ main.py
@@ -1,4 +1,11 @@
-from google import get_links_for_name
+from google import get_numbers_and_links_for_name
+from basic_search import find_numbers_on_page
 
 name = input("Please input your full name to search: ").strip()
-get_links_for_name(name)
+google_links, link_grp = get_numbers_and_links_for_name(name)
+print('https://www.google.com/:', google_links if google_links else 'No phone numbers found')
+
+search_links_goog = [link for grp in link_grp.values() for link in grp]
+for link in search_links_goog:
+    print(link, end=": ")
+    print(find_numbers_on_page(link))
